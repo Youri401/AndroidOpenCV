2e2d42455a60cd337a9106bf96c804340ae1ac9f
sakachi <youri401@yahoo.co.jp>
2019-08-12 13:34:23 +0900

    first commit

---
+++ app/src/main/java/com/example/opencvdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.opencvdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(!OpenCVLoader.initDebug()){
+            Log.i("OpenCV","failed");
+        }
+        else{
+            Log.i("OpenCV","successfull");
+        }
+    }
+}
