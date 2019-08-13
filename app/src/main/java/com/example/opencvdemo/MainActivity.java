7ae536ec7d3ad239b4305653db4c697b1ae74839
sakachi <youri401@yahoo.co.jp>
2019-08-13 12:46:15 +0900

    add reading permission

--- app/src/main/java/com/example/opencvdemo/MainActivity.java
+++ app/src/main/java/com/example/opencvdemo/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.opencvdemo;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.opencv.android.OpenCVLoader;
 
@@ -20,11 +29,19 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private static final int PICK_VIDEO_REQUEST = 10;
+    private static final int REQUEST_CODE_PERMISSION = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(this,"Permission is off",Toast.LENGTH_SHORT).show();
+            }else{
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},REQUEST_CODE_PERMISSION);
+            }
+        }
 
         if(!OpenCVLoader.initDebug()){
             Log.i("OpenCV","failed");
@@ -48,17 +65,78 @@ public class MainActivity extends AppCompatActivity {
         if(resultData != null) {
             Log.i("intent_info", resultData.getData().toString());
             Log.i("intent_info", this.getApplicationContext().toString());
+            Log.i("intent_info", getPathFromUri(this.getApplicationContext(),resultData.getData()));
+
         }
         else{
             Log.i("intent_info", "null");
         }
     }
 
-    public boolean onKeyDown(int keyCode, KeyEvent event){
-        if(keyCode != KeyEvent.KEYCODE_BACK){
-            return super.onKeyDown(keyCode,event);
-        }else{
-            return false;
+    public String getPathFromUri(final Context context, final Uri uri) {
+        boolean isAfterKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+        // DocumentProvider
+        Log.e("intent_info","uri:" + uri.getAuthority());
+        if (isAfterKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            if ("com.android.externalstorage.documents".equals(
+                    uri.getAuthority())) {// ExternalStorageProvider
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }else {
+                    return "/stroage/" + type +  "/" + split[1];
+                }
+            }else if ("com.android.providers.downloads.documents".equals(
+                    uri.getAuthority())) {// DownloadsProvider
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                Log.i("intent_info", "ok1");
+                return getDataColumn(context, contentUri, null, null);
+            }else if ("com.android.providers.media.documents".equals(
+                    uri.getAuthority())) {// MediaProvider
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                Uri contentUri = null;
+                contentUri = MediaStore.Files.getContentUri("external");
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+                Log.i("intent_info", "ok2");
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }else if ("content".equalsIgnoreCase(uri.getScheme())) {//MediaStore
+            return getDataColumn(context, uri, null, null);
+        }else if ("file".equalsIgnoreCase(uri.getScheme())) {// File
+            return uri.getPath();
         }
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+        Cursor cursor = null;
+        final String[] projection = {
+                MediaStore.Files.FileColumns.DATA
+        };
+        try {
+            cursor = context.getContentResolver().query(
+                    uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int cindex = cursor.getColumnIndexOrThrow(projection[0]);
+                return cursor.getString(cindex);
+            }
+        }catch (Exception e){
+            Log.i("intent_info", e.toString());
+        }
+        finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
     }
 }
